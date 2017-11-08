cced9415e90d23e8803dbdf0ae557d48e8d92155
b-i-hide <b.i1993patriots@gmail.com>
2017-11-08 17:10:57 +0900

    [add] tweets controllerを追加

--- controllers/tweets_controller.go
+++ controllers/tweets_controller.go
@@ -1,1 +1,17 @@
 package controllers
+
+import (
+	"database/sql"
+	"github.com/gin-gonic/gin"
+	"net/http"
+)
+
+type Tweet struct {
+	DB *sql.DB
+}
+
+func (s Tweet) Index(c *gin.Context) {
+	c.HTML(http.StatusOK, "tweets_index", gin.H{
+		"title": "Index",
+	})
+}
